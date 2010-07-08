708c1e2b67773058dd01b84f25e10476730cd5ff
zuke <daniel.zukowski@gmail.com>
2010-07-08 01:16:45 -0600

    Stubbed out functions for programs and publications. To test, go to /main/programs or main/publications in your browser.
    Signed-off-by: zuke <daniel.zukowski@gmail.com>

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -8,5 +8,24 @@ class Main extends Controller {
     
     public function index() {
         $this->load->view('http_header', array('title' => 'NITA - National Institute for Trial Advocacy'));
+        $this->load->view('header');
+        $this->load->view('home/default');
+        $this->load->view('footer');
+    }
+    
+    public function programs() {
+        $this->load->view('http_header', array('title' => 'NITA - National Institute for Trial Advocacy'));
+        $this->load->view('header');
+        $this->load->view('main_nav');
+        $this->load->view('programs/list');
+        $this->load->view('footer');
+    }
+    
+    public function publications() {
+        $this->load->view('http_header', array('title' => 'NITA - National Institute for Trial Advocacy'));
+        $this->load->view('header');
+        $this->load->view('main_nav');
+        $this->load->view('publications/list');
+        $this->load->view('footer');
     }
 }
