a428419983eba89d4f67875b4ada1017a6c7bed8
Chris Boertien <chris.boertien@gmail.com>
2010-07-08 02:48:03 -0600

    Began tying in controller to models
    Right now only the Publication model is producing any data.
    
    Also did some reworking of the view setup to help reduce code
    duplication. Not sure if this is going to be final but it should
    make maintaining that a touch easier.
    
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -1,31 +1,67 @@
 <?php
 
 class Main extends Controller {
+
+    // the page title
+    private $title;
     
     public function __construct() {
         parent::Controller();
     }
     
     public function index() {
-        $this->load->view('http_header', array('title' => 'NITA - National Institute for Trial Advocacy'));
-        $this->load->view('header');
-        $this->load->view('home/default');
-        $this->load->view('footer');
+        $this->title = 'NITA - National Institue for Trial Advocacy';
+        $views = array(
+            array('name' => 'home/default', 'args' => null)
+        );
+        
+        $this->loadViews($views);
     }
     
     public function programs() {
-        $this->load->view('http_header', array('title' => 'NITA - National Institute for Trial Advocacy'));
-        $this->load->view('header');
-        $this->load->view('main_nav');
-        $this->load->view('programs/list');
-        $this->load->view('footer');
+        $this->title = 'Nita - Our Programs';
+        
+        // Load the model and get some data
+        $this->load->model('program');
+        
+        // Gives an array of Program objects
+        // MockSoap not setup for this yet...
+        //$programs = $this->program->getAllPrograms();
+        $programs = array();
+        
+        $views = array(
+            array('name' => 'main_nav', 'args' => null),
+            array('name' => 'programs/list', 'args' => $programs)
+        );
+        
+        $this->loadViews($views);
     }
     
     public function publications() {
-        $this->load->view('http_header', array('title' => 'NITA - National Institute for Trial Advocacy'));
+        $this->title = 'NITA - Our Publications';
+        
+        // Load the model and get some data
+        $this->load->model('publication');
+        
+        // Gives an array of Publication objects
+        $publications = $this->publication->getAllPublications();
+        
+        $views = array(
+            array('name' => 'main_nav', 'args' => null),
+            array('name' => 'publications/list', 'args' => $publications)
+        );
+        
+        $this->loadViews($views);
+    }
+    
+    private function loadViews(array $views) {
+        $this->load->view('http_header', array('title' => $this->title));
         $this->load->view('header');
-        $this->load->view('main_nav');
-        $this->load->view('publications/list');
+        foreach($views as $view) {
+            $this->load->view($view['name'], $view['args']);
+        }
         $this->load->view('footer');
     }
 }
+
+/* End of file main.php */
