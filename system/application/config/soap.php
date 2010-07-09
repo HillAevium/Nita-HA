bdda43de4d172c5f67c3b3fafc3e16d8331e1ad2
Chris Boertien <chris.boertien@gmail.com>
2010-07-08 22:22:19 -0600

    Moved some of the Soap settings to the soap config file
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- system/application/config/soap.php
+++ system/application/config/soap.php
@@ -13,7 +13,7 @@
 |       http://10.42.122.19/
 |
 */
-$soap['server'] = '';
+$soap['server'] = 'http://10.137.1.49:6652/';
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +25,20 @@ $soap['server'] = '';
 |
 |       SomeWsdlService.svc?wsdl
 */
-$soap['wsdl']   = '';
+$soap['services']['webPages'] = 'WebPageService.svc?wsdl';
+$soap['services']['programs'] = 'ProgramService.svc?wsdl';
+$soap['services']['users']    = 'AccountService.svc?wsdl';
+$soap['services']['contacts'] = 'ContactService.svc?wsdl';
+
+
+/*
+|--------------------------------------------------------------------------
+| Soap Page IDs
+|--------------------------------------------------------------------------
+|
+| Page ID numbers for contentent coming from CRM.
+*/
+$soap['page']['home']   = 'f4d317f9-f37e-df11-8d9f-000c2916a1cb';
+$soap['page']['footer'] = '582cd7a9-8f80-df11-8d9f-000c2916a1cb';
 
 /* End of file soap.php */
