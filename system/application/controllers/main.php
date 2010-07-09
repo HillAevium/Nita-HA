69249634097471732a102db685e51c921fbd09f9
Chris Boertien <chris.boertien@gmail.com>
2010-07-09 05:23:51 -0600

    Performed some surgery on the Main controller
    Added arguments to the various entry points to read the URI segments
    Created a function for the creation of pagination links
    Created an args array to store parameters for the views
    Added some skeleton docs for the entry points
    
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -1,118 +1,187 @@
 <?php
 
 class Main extends Controller {
-
+    
+    /* TODO
+     * > Should we move the generic page titles to a config file?
+     */
+    
     // the page title
     private $title;
     
     public function __construct() {
         parent::Controller();
+        MockSoap::$numberOfItems = 65;
     }
     
+    /**
+     * The site home page.
+     *
+     * This entry point will be used when there is not
+     * a complete URI path to map to any other entry point.
+     */
     public function index() {
         $this->title = 'NITA - National Institue for Trial Advocacy';
+        
+        // Setup the views
         $views = array(
             array('name' => 'home/default', 'args' => null)
         );
         
+        // ... and go
         $this->loadViews($views);
     }
     
-    public function program() {
+    /**
+     * Generates the page for displaying a specific program.
+     *
+     * @param string $id the id of the program to display
+     */
+    public function program($id = '') {
+        if($id === '') {
+            // What do we do here?
+            show_404('/main/programs/');
+        }
+        
         // Load the model and get some data
         $this->load->model('program');
         
-        // program id
-        $id = $this->uri->segment(3);
+        // Returns a single Program object
+        $args['program'] = $this->program->getProgram($id);
         
-        $program = $this->program->getProgram($id);
+        // We need to check if we got a program as the ID could
+        // have been entered by hand and been invalid
+        if (is_null($args['program'])) {
+            // Return a 404?
+            show_404('/main/program/'.$id);
+        }
         
+        // Set the title to the Program being viewed
+        $this->title = $args['program']->name;
+        
+        // Setup the views
         $views = array(
             array('name' => 'main_nav', 'args' => null),
-            array('name' => 'programs/detail', 'args' => array(
-              'program'  => $program)
-            )
+            array('name' => 'programs/detail', 'args' => $args)
         );
         
+        // ... and go
         $this->loadViews($views);
-        
     }
     
-    public function programs() {
+    /**
+     * Generate the page for displaying a list of programs.
+     *
+     * @param int $offset the current offset within the list of items to show
+     */
+    public function programs($offset = 0) {
         $this->title = 'Nita - Our Programs';
         
         // Load the model and get some data
         $this->load->model('program');
         
         // Gives an array of Program objects
-        // MockSoap not setup for this yet...
-        //$programs = $this->program->getAllPrograms();
-        $programs = array();
+        $args['programs']   = $this->program->getAllPrograms();
+        $args['pagination'] = $this->createPaginationLinks(__FUNCTION__, count($args['programs']));
         
+        // TODO - We should do something here if no items are returned
+        
+        // Slice the programs array to the range
+        // called for by the pagination
+        $args['programs'] = array_slice($args['programs'], $offset, $this->pagination->per_page);
+        
+        // Setup the views
         $views = array(
             array('name' => 'main_nav', 'args' => null),
-            array('name' => 'programs/list', 'args' => array('programs' => $programs))
+            array('name' => 'programs/list', 'args' => $args)
         );
         
+        // ... and go
         $this->loadViews($views);
     }
-
-    public function publication() {
+    
+    /**
+     * Generate the page for displaying a specific publication.
+     *
+     * @param string $id the id of the publication to display
+     */
+    public function publication($id = '') {
+        if($id === '') {
+            // What do we do here?
+            show_404('/main/publication/');
+        }
+        
         // Load the model and get some data
         $this->load->model('publication');
         
-        // publication id
-        $id = $this->uri->segment(3);
+        // Returns a single Publication object
+        $args['publication'] = $this->publication->getPublication($id);
         
-        $publication = $this->publication->getPublication($id);
+        // We need to check if we got a publication as the ID could
+        // have been entered by hand and been invalid
+        if (is_null($args['publication'])) {
+            // Return a 404?
+            show_404('/main/program/'.$id);
+        }
         
+        // Set the title to the publication being viewed
+        $this->title = $args['publication']->title;
+        
+        // Setup the views
         $views = array(
             array('name' => 'main_nav', 'args' => null),
-            array('name' => 'publications/detail', 'args' => array(
-              'publication'  => $publication)
-            )
+            array('name' => 'publications/detail', 'args' => $args)
         );
         
+        // ... and go
         $this->loadViews($views);
-        
     }
     
-    public function publications() {
+    /**
+     * Generate the page for displaying a list of publications.
+     *
+     * @param int $offset the current offset within the list of items to show
+     */
+    public function publications($offset = 0) {
         $this->title = 'NITA - Our Publications';
         
-        // Load the model and get some data
+        // Load the model
         $this->load->model('publication');
         
         // Gives an array of Publication objects
-        $publications = $this->publication->getAllPublications();
+        $args['publications'] = $this->publication->getAllPublications();
         
-        $this->load->library('pagination');
-
-        // Set the pagination configuration
-        // NOTE: These pagination routines should probably
-        //       be moved elsewhere, into a helper or something.
-        $paginationConfig = array();
-        $paginationConfig['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/main/' . __FUNCTION__ . '/';
-        $paginationConfig['total_rows'] = count($publications);
-
-        $this->pagination->initialize($paginationConfig);
-
-        $pagination = $this->pagination->create_links();
+        // Create the pagination links
+        $args['pagination'] = $this->createPaginationLinks(__FUNCTION__, count($args['publications']));
+        
+        // TODO - We should do something here if no items are returned
         
         // Slice the publications array to the range
         // called for by the pagination
-        $offset = $this->uri->segment(3) ? $this->uri->segment(3) : 0;
-        $publications = array_slice($publications, $offset, $this->pagination->per_page);
+        $args['publications'] = array_slice($args['publications'], $offset, $this->pagination->per_page);
         
+        // Setup the views
         $views = array(
             array('name' => 'main_nav', 'args' => null),
-            array('name' => 'publications/list', 'args' => array(
-              'pagination'    => $pagination,
-              'publications'  => $publications)
-            )
+            array('name' => 'publications/list', 'args' => $args)
         );
         
+        // ... and go
         $this->loadViews($views);
+    }
+    
+    private function createPaginationLinks($function, $totalRows) {
+        $this->load->library('pagination');
+        
+        // Move this to config/pagination.php ?
+        $config = array();
+        $config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/main/' . $function;
+        $config['total_rows'] = $totalRows;
+        $config['per_page'] = 10;
+        
+        $this->pagination->initialize($config);
+        
+        return $this->pagination->create_links();
     }
     
     private function loadViews(array $views) {
