a3730cf87995f05c3ff505e2f3ed1aac3f168a67
zuke <daniel.zukowski@gmail.com>
2010-07-09 01:43:50 -0600

    New views for detail pages

---
+++ system/application/views/programs/detail.php
@@ -0,0 +1,3 @@
+<div id="content_main">
+This is the program detail page.
+</div> <!-- #content_main -->
---
+++ system/application/views/publications/detail.php
@@ -0,0 +1,3 @@
+<div id="content_main">
+This is the publication detail page.
+</div> <!-- #content_main -->
