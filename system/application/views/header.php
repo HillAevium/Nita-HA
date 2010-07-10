d9033477148e254956fade5de83deb5a2a43c33f
zuke <daniel.zukowski@gmail.com>
2010-07-10 13:55:16 -0600

    Replaced header images with final design slices. Hover states sliced but not activated. These will be handled with jquery.

--- system/application/views/header.php
+++ system/application/views/header.php
@@ -3,7 +3,15 @@
 <div id="header">
   <a id="logo" href="/"></a>
   <div id="title">National Institution for Trial Advocacy</div>
-  <div id="header_tools">
-    <input id="header_search" type="text" />
+  <div id="search_bar">
+    <div id="search_bar_inner">
+      <div id="search_bar_search">
+        <input name="search" type="text" />
+      </div>
+      <a id="search_bar_rss" class="search_bar_rss" href=""></a>
+      <a id="search_bar_cart" class="search_bar_cart" href=""></a>
+      <a id="search_bar_account" class="search_bar_account" href=""></a>
+      <a id="search_bar_print" class="search_bar_print" href=""></a>
+    </div>
   </div>
 </div> <!-- #header -->
