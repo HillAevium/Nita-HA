3123e5b9865cc835d26eeab286578c1c8225a1f5
Chris Boertien <chris.boertien@gmail.com>
2010-07-12 17:56:32 -0600

    Fixed main_nav links
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- system/application/views/main_nav.php
+++ system/application/views/main_nav.php
@@ -1,11 +1,11 @@
 <div id="main_nav">
   <ul class="menu">
-    <li><a class="programs" href="shop/programs"></a></li>
+    <li><a class="programs" href="/shop/programs"></a></li>
     <li><a class="publications" href="/shop/publications"></a></li>
     <li><a class="custom_programs" href="/shop/customprograms"></a></li>
-    <li><a class="law_schools" href="main/law_schools"></a></li>
-    <li><a class="donate" href="shop/donate"></a></li>
-    <li><a class="about" href="main/about"></a></li>
-    <li><a class="contact" href="main/contact"></a></li>
+    <li><a class="law_schools" href="/main/law_schools"></a></li>
+    <li><a class="donate" href="/shop/donate"></a></li>
+    <li><a class="about" href="/main/about"></a></li>
+    <li><a class="contact" href="/main/contact"></a></li>
   </ul>
 </div> <!-- #main_nav -->
