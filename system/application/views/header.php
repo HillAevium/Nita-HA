32b474b94c97d32376704b32d8266dd47804f3bf
zuke <daniel.zukowski@gmail.com>
2010-07-14 15:00:15 -0600

    The has_hover class will be used by jQuery to recognize elements whose backgrounds will swap to an image with _over at the end of the file name. jQuery can then preload these images to prepare for the rollover.
    Signed-off-by: Daniel Zukowski <daniel.zukowski@gmail.com>

--- system/application/views/header.php
+++ system/application/views/header.php
@@ -9,10 +9,10 @@
                 <div id="search_bar_search">
                     <input name="search" type="text" />
                 </div>
-                <a id="search_bar_rss" class="search_bar_rss" href=""></a>
-                <a id="search_bar_cart" class="search_bar_cart" href=""></a>
-                <a id="search_bar_account" class="search_bar_account" href=""></a>
-                <a id="search_bar_print" class="search_bar_print" href=""></a>
+                <a id="search_bar_rss" class="search_bar_rss has_hover" href=""></a>
+                <a id="search_bar_cart" class="search_bar_cart has_hover" href=""></a>
+                <a id="search_bar_account" class="search_bar_account has_hover" href=""></a>
+                <a id="search_bar_print" class="search_bar_print has_hover" href=""></a>
             </div>
         </div>
     </div> <!-- #header_inner -->
